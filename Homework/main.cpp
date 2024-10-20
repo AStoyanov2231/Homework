f7eff8633d501ef85c39551b5a80f7c6ac64a6c1
Andy-Unity <andysonyz4aqua@gmail.com>
2024-10-20 18:38:16 +0300

    Class Homework_1

--- Homework/main.cpp
+++ Homework/main.cpp
@@ -1,50 +1,19 @@
-
-
+#include "Homework_1.h"
 #include <iostream>
 #include <string>
 
-using namespace std;
+int main() {
 
-int main()
-{
-	string email = "";
+    Homework_1 homework_1;
 
-	cin >> email;
+    cout << "Zadacha 1\n";
 
-	size_t atPosition = email.find('@');
-	string user = email.substr(0, atPosition);
-	string server = email.substr(atPosition + 1);
+    homework_1.ZadachaEmail();
+    cout << endl;
+    cout << "Zadacha 2\n";
+    cout << endl;
 
-	string nationality = (server.find(".bg") == server.length() - 3) ? "bulgarski e" : "ne e bulgarski";
+    homework_1.ZadachaURL();
 
-	cout << user << endl;
-	cout << server << endl;
-	cout << nationality;
-
-	////////////////////////////////////////////
-
-	string URL = "";
-
-	cin >> URL;
-
-	size_t protocolEnd = URL.find("://");
-
-	string protocol = URL.substr(0, protocolEnd);
-
-	size_t hostStart = protocolEnd + 3;
-	size_t pathStart = URL.find("/", hostStart);
-
-	string host = (pathStart != string::npos) ? URL.substr(hostStart, pathStart - hostStart) : URL.substr(hostStart);
-
-	string file, path;
-	if (pathStart != string::npos) {
-		size_t fileStart = URL.rfind("/");
-		path = URL.substr(pathStart, fileStart - pathStart);
-		file = URL.substr(fileStart + 1);
-	}
-
-	cout << "Protocol: " << protocol << endl;
-	cout << "Host: " << host << endl;
-	cout << "Path: " << path << endl;
-	cout << "File: " << file << endl;
+    return 0;
 }
