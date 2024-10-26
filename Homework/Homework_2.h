958d36561b89f0ee523a37f3588bb71906ef2da0
Andy-Unity <andysonyz4aqua@gmail.com>
2024-10-26 18:17:38 +0300

    homework 2

--- Homework/Homework_2.h
+++ Homework/Homework_2.h
@@ -1,1 +1,58 @@
 #pragma once
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Homework_2 {
+public:
+	void Zadacha1() {
+
+        string inputText;
+        cout << "Enter text: ";
+        getline(cin, inputText);
+
+        string result;
+        string mask = " ,.;!?";
+        size_t start = 0, end = 0;
+        int wordCount = 1;
+
+        while ((start = inputText.find_first_not_of(mask, end)) != string::npos) {
+            end = inputText.find_first_of(mask, start);
+            string word = inputText.substr(start, end - start);
+
+            if (word.find_first_not_of("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ") == string::npos) {
+                result += "word" + to_string(wordCount);
+                wordCount++;
+            }
+            else {
+                result += word;
+            }
+
+            if (end != string::npos && mask.find(inputText[end]) != string::npos) {
+                result += inputText[end];
+                if (inputText[end] == ',') {
+                    result += " ";
+                }
+            }
+        }
+
+        cout << result << endl;
+	}
+
+    void Zadacha2() {
+
+        string inputText;
+        cout << "Enter text: ";
+        getline(cin, inputText);
+
+        size_t pos = -1;
+
+        while ((pos = inputText.find_first_of("0123456789", pos + 1)) != string::npos) {
+            size_t end = inputText.find_first_not_of("0123456789", pos);
+            inputText.replace(pos, end - pos, "*");
+        }
+
+        cout << inputText << endl;
+    }
+};
--- Homework/main.cpp
+++ Homework/main.cpp
@@ -1,19 +1,17 @@
 #include "Homework_1.h"
+#include "Homework_2.h"
 #include <iostream>
 #include <string>
 
 int main()
 {
-    Homework_1 homework_1;
+    Homework_2 homework_2;
 
     cout << "Zadacha 1\n";
-
-    homework_1.ZadachaEmail();
+    homework_2.Zadacha1();
     cout << endl;
     cout << "Zadacha 2\n";
-    cout << endl;
-
-    homework_1.ZadachaURL();
+    homework_2.Zadacha2();
 
     return 0;
 }
